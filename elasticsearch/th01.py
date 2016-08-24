835cb1d9f914e7b3f432552b1666558f21e7fec4
zhengyansheng <467754239@qq.com>
2016-08-24 18:35:05 +0800

    add es

--- elasticsearch/th01.py
+++ elasticsearch/th01.py
@@ -1,17 +1,93 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from datetime import datetime
+import sys
+import time
+import json
+import redis
+import logging
+
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
+from elasticsearch.helpers import bulk, parallel_bulk
 
-msg = '''
-219.232.105.97 - - [23/Aug/2016:08:20:31 +0000] "GET /bundles/node_modules/font-awesome/fonts/fontawesome-webfont.woff2 HTTP/1.1" 304 0 "http://52.38.135.142:8000/bundles/statusPage.style.css?v=9689" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
-'''
+class elasctic(object):
+    def __init__(self, host='127.0.0.1', port=9200, flush_size=2000):
+        self.host = host
+        self.port = port 
+        self.conn = None
+        self.flush_size = flush_size
 
-bulk_data = []
-for id in xrange(20000):
-     bulk_data.append({"_index" : "sengled", "_type" : "nginx", "_id" : id, 'timestamp':datetime.now(), "doc" : msg})
+    def connect(self):
+        self.conn = Elasticsearch([
+                        {'host' : self.host}, 
+                        {'port' : self.port}
+                        ])
 
-client = Elasticsearch([{'host' : 'localhost'}, {'port' : 9200}])
-bulk(client, bulk_data)
+    def bulkdata(self, data):
+        self.connect()
+        # bulk(client=self.conn, actions=data, stats_only=True)
+        parallel_bulk(client=self.conn, actions=data, thread_count=4, chunk_size=500)
+
+
+class Redis(object):
+    def __init__(self, host='127.0.0.1', port=6379, db=0):
+        self.host = host
+        self.port = port
+        self.db = db
+        self.r = None
+        self.es = elasctic()
+
+    def connect(self):
+        self.r = redis.Redis(host=self.host, port=self.port, db=self.db)
+
+    def pop_from_redis(self, key):
+        return self.r.lpop(key)
+
+    def get_queue_length(self, key):
+        return self.r.llen(key)
+
+    def format_data(self, data):
+        jsondata = json.loads(data)
+        format_data = {
+                "_index" : 'zhengys',
+                "_type" : jsondata['type'],
+                "timestamp" : jsondata['@timestamp'],
+                "host" : jsondata['host'],
+                "public_ip" : jsondata['public_ip'],
+                "path" : jsondata['path'],
+                "message" : jsondata['message']
+                }
+        return format_data
+
+    def process(self, key):
+        self.connect()
+        
+        cache = []
+        interval = 5 
+
+        while True:
+            length = self.get_queue_length(key)
+            if length == 0:
+                logging.debug('key:%s, pop finish.' % self.key)
+                time.sleep(interval)
+            else:
+                value_str = self.pop_from_redis(key)
+                format_record = self.format_data(value_str)
+                cache.append(format_record)
+
+                if len(cache) == self.es.flush_size:
+                    time.sleep(interval)
+                    self.es.bulkdata(cache)
+                    logging.debug('flush_size:%s, bulk data to es finish.' % self.es.flush_size)
+                    cache = []
+                    continue
+                
+
+def main():
+    r = Redis(host='127.0.0.1', port=6380)
+    r.process("logstash:redis")
+
+if __name__ == '__main__':
+    FORMAT = '%(asctime)-15s %(filename)s line:%(lineno)d %(levelname)s %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+    main()
