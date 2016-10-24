8221acf50ce8dd2299c792d2b337535d950e521a
zhengyansheng <467754239@qq.com>
2016-10-24 15:38:50 +0800

    add ppty.py

--- builtin_module/ppty.py
+++ builtin_module/ppty.py
@@ -1,57 +1,61 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import sys
+
 import os
 import pty
 import select
-import time
 
-class selectPty(object):
+class selectFork(object):
     def __init__(self):
         self._setupPty()
 
     def _setupPty(self):
-        (pid, master_fd) = pty.fork()
+        (pid, master_fd)= pty.fork()
         self.stdout = master_fd
         self.stdin = master_fd
-        self.stderr = master_fd
-
+        self.stdout = master_fd
         if pid == 0:
             os.execv("/bin/bash",["/bin/bash","-l"])
 
-    def exec_command(self, cmd):
-        cmd = cmd.strip() + os.linesep
-        self.write(cmd)
-
     def read(self):
-        rlist = [self.stdout]
+        rlist = [ self.stdout ]
         wlist = []
         xlist = []
 
-        timeout = 0.5
-        output =""
+        output = ""
+        timeout = 0.5 
 
-        while 1:
+        while True:
+            # select(rlist, wlist, xlist[, timeout]) -> (rlist, wlist, xlist)
             rlist, wlist, xlist = select.select(rlist, wlist, xlist, timeout)
             for r in rlist:
                 tmp = os.read(self.stdout, 1024)
                 output += tmp
                 if tmp == "":
-                    r=[]
+                    r = []
                     break
+
             if not len(rlist) and len(output):
                 return output.split(os.linesep)
+                break
 
-    def write(self, cmd):
+    def write(self,cmd):
+        cmd = cmd.strip() + os.linesep
         os.write(self.stdin, cmd)
 
 def main():
-    cmdList=["ls","uptime","df -h", 'cat /etc/passwd', 'dfs']
-    zt = selectPty()
-    for cmd in cmdList:
-        zt.exec_command(cmd)
-        lines=zt.read()
+    commands = '''
+    ls
+    uptime
+    cat /etc/passwd
+    df -h
+    ''' 
+
+    zt = selectFork()
+    for cmd in commands.strip().split('\n'):
+        zt.write(cmd)
+        lines = zt.read()
         for line in lines:
             print line
    
